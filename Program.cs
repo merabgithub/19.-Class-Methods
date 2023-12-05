70a28185dc12e8cf7809d782732fd48771cb52d9
MercyM <MercyM@S-MERCYM-Z15UG6>
2023-12-05 20:32:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            Student student2 = new Student(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            //freese console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_Methods
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;// the word this is an explicit way of
+            this.name = aName;
+            this.age = aAge; 
+        }
+
+        //You now have one stop shop to test obesity of your students
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
